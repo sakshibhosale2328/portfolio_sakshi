27944ea7149a175d0193069fbb420c4517f75553
Sakshi Yewale <sakshiyewale23@gmail>
2025-07-30 16:10:23 +0530

    Fix: Correct index.html route in vercel.json

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0bde5ba460b965ed015a35d18d9e03bf
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "9a706e7ff01e392b23ebe9d648591a9f",
+"flutter_bootstrap.js": "6dd6113a6214def70e131096d40a9556",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
